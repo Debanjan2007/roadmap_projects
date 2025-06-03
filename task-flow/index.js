3c4bfa49bb8942ac081b764ab716bec144376f26
Debanjan Das <microsoft2420@gmail.com>
2025-06-03 23:45:08 +0530

    Refactor addTask function to improve task handling and file creation logic

--- task-flow/index.js
+++ task-flow/index.js
@@ -1,10 +1,36 @@
+const { error } = require('console');
 const fs = require('fs');
 const path = require('path');
 
 const taskfile = path.join(__dirname , 'taskfile.json') ;
 
 
+
 //  funstion for adding a task
-const addTask = (task) => {
+function addTask(task) {
+    if(!task){
+        return console.log('Please provide a task to add');
+    }
+
+    if(!fs.existsSync(taskfile)){
+        fs.writeFileSync(taskfile , JSON.stringify([]) , 'utf-8' , (err , data) => {
+            if(err){
+                throw new err
+            }
+            console.log('Task file created');
+        })
+        
+    }
+
+    const tasks = fs.readFileSync(taskfile , 'utf-8' , (err , data) =>{
+        if(err){
+            throw new err;
+        }
+        console.log(data);
+    })
+    const taskId = JSON.parse(tasks)
+    console.log(typeof taskId);
+    
     
 }
+addTask(process.argv.slice(2)[1]) ;
