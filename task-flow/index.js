549f48a31f6d1d67607207c65f1ec94a5cb6518a
Debanjan Das <microsoft2420@gmail.com>
2025-06-03 18:12:55 +0530

    Initialize task-flow project with basic structure and configuration files

---
+++ task-flow/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const taskfile = path.join(__dirname , 'taskfile.json') ;
+
+
+//  funstion for adding a task
+const addTask = (task) => {
+    
+}
