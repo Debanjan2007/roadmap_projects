1c73028208300b2ddfa4902334a7113edc41de5f
Debanjan Das <microsoft2420@gmail.com>
2025-06-06 23:48:36 +0530

    feat: enhance addTask function with error handling and success messages

--- task-flow/index.js
+++ task-flow/index.js
@@ -7,7 +7,7 @@ const pathOperation = process.argv.slice(2)?.[0] ;
 // adds a task to the tasks.json file
 const addTask = (task) => {
     if(!task){
-        return console.log("Please enter a task to add");
+        return console.log("⚠ Please enter a task to add ⚠");
     }
     if(!fs.existsSync('tasks.json')){
         const tasks = [
@@ -18,12 +18,18 @@ const addTask = (task) => {
             }
         ] ; 
         fs.writeFileSync('tasks.json' , JSON.stringify(tasks , null , 2)) ; 
-        return console.log("Task added successfully");
+        return console.log("Task added successfully \n taskId : 1");
     }
     const fetchedTasks = JSON.parse(fs.readFileSync('tasks.json' , 'utf-8' , (error) => {
         console.log("Error reading tasks.json file: " + error);
     })) ;
-    const todoId = Object.keys(fetchedTasks).length ; 
+    const todoId = Object.keys(fetchedTasks).length ; // the length of the todo s already added 
+    for(i = 0 ; i < todoId ; i++){
+        if(fetchedTasks[i].task == task){
+        return console.log("⚠ You are trying to add same task again ⚠ \n Your task exists with id : ",(i+1));
+        }
+    }
+    
     const newTask = {
         id: todoId + 1 ,
         task: task ,
@@ -31,12 +37,32 @@ const addTask = (task) => {
     } ;
     fetchedTasks.push(newTask) ;
     fs.writeFileSync('tasks.json' , JSON.stringify(fetchedTasks , null , 2)) ;
-    console.log("Task added successfully");
-    
+    console.log(`✅ Task added successfully ✅ \n taskId : ${newTask.id} `);
 }
 // updates task status to done or todo
-const updateTask = (taskId , newTask) => {
-    console.log("updating task with id: " + taskId + " to new task: " + newTask);
+const updateTask = async (taskId , newTask) => {
+    if(!newTask || !taskId){
+        return console.log("⚠ make sure you enter a valid index or update msg ⚠");
+    }
+    const tasks = JSON.parse(fs.readFileSync('tasks.json' , 'utf-8' , (err) => {
+        console.log(err);
+        return ;
+    })) ;
+    const tasktoupdate = tasks[taskId - 1] ; // getting the requested task
+    if(!tasktoupdate){
+        return console.log("⚠ No task found with that index ⚠");
+    }
+    const updatedtask = {
+        id: index.id ,
+        task: newTask ,
+        status: 'todo'
+    }
+    tasks.splice(index.id - 1 , 1 , updatedtask) ;
+    fs.writeFile('tasks.json' , JSON.stringify(tasks , null , 2) , (err) => {
+        if(err)
+            console.log(err);
+    }) ;
+    console.log("update complete");
 }
 
 // deletes task by id
