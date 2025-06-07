831a74d8b7cf6e6d4d648eb052e1f048ba8960a5
Debanjan Das <microsoft2420@gmail.com>
2025-06-07 18:36:14 +0530

    feat: enhance addTask and updateTask functions with improved error messages and add list functionalities

--- task-flow/index.js
+++ task-flow/index.js
@@ -1,4 +1,3 @@
-const { error } = require('console');
 const fs = require('fs');
 
 
@@ -7,7 +6,7 @@ const fs = require('fs');
 // adds a task to the tasks.json file
 const addTask = (task) => {
     if(!task){
-        return console.log("⚠ Please enter a task to add ⚠");
+        return console.log("⚠️️️️️️ Please enter a task to add ⚠️️️️️️");
     }
     if(!fs.existsSync('tasks.json')){
         const tasks = [
@@ -26,7 +25,7 @@ const addTask = (task) => {
     const todoId = Object.keys(fetchedTasks).length ; // the length of the todo s already added 
     for(i = 0 ; i < todoId ; i++){
         if(fetchedTasks[i].task == task){
-        return console.log("⚠ You are trying to add same task again ⚠ \n Your task exists with id : ",(i+1));
+        return console.log("⚠️️️️️️ You are trying to add same task again ⚠️️️️️️ \n Your task exists with id : ",(i+1));
         }
     }
     
@@ -38,13 +37,16 @@ const addTask = (task) => {
     fetchedTasks.push(newTask) ;
     fs.writeFileSync('tasks.json' , JSON.stringify(fetchedTasks , null , 2)) ;
     console.log(JSON.stringify(newTask , null , 2));
-    console.log(`✅ Task added successfully ✅ \n taskId : ${newTask.id} `);
+    console.log(`✔️Task added successfully ✔️\n taskId : ${newTask.id} `);
 }
 // updates task status to done or todo
 const updateTask = async (taskId , newTask) => {
     if(!newTask || !taskId){
-        return console.log("⚠ make sure you enter a valid index or update msg ⚠");
+        return console.log("⚠️️️️️️ make sure you enter a valid index or update msg ⚠️️️️️️");
     }
+     if(!fs.existsSync('tasks.json')){
+            return console.log("⚠️ There is no file for todos! ⚠️");        
+        }
     const tasks = JSON.parse(fs.readFileSync('tasks.json' , 'utf-8' , (err) => {
         console.log(err);
         return ;
@@ -54,7 +56,7 @@ const updateTask = async (taskId , newTask) => {
     }
     const tasktoupdate = tasks[taskId - 1] ; // getting the requested task
     if(!tasktoupdate){
-        return console.log("⚠ No task found with that index ⚠");
+        return console.log("⚠️️️️️ No task found with that index ⚠️️️️️️");
     }
     const updatedtask = {
         id: tasktoupdate.id ,
@@ -66,7 +68,7 @@ const updateTask = async (taskId , newTask) => {
         if(err)
             console.log(err);
     }) ;
-    console.log("✅ update complete ✅");
+    console.log("✔️update complete ✅");
 }
 
 // deletes task by id
@@ -74,6 +76,9 @@ const deltask = (taskId) => {
     if(!taskId || taskId < 0){
         return console.log("Enter right id to delete task");
     }
+     if(!fs.existsSync('tasks.json')){
+            return console.log("⚠️ There is no file for todos! ⚠️");        
+        }
     const tasks = JSON.parse(fs.readFileSync('tasks.json' , 'utf-8' , (err) => {
         if(err)
             console.log(err);
@@ -87,11 +92,88 @@ const deltask = (taskId) => {
     console.log("todo deleted with the id : " , taskId);
 }
 
+const listTodo = () => {
+    try {
+        if(!fs.existsSync('tasks.json')){
+            return console.log("⚠️ There is no file for todos! ⚠️");        
+        }
+        const todos = JSON.parse(fs.readFileSync('tasks.json' , 'utf-8' ,  (err) => {
+            if(err){
+                throw new err ;
+            }
+        })) ;
+        // taskLen = Object.keys(tasks).length ;
+        let tasks = [] ; 
+        todos.forEach(elm => {
+            if(elm.status === "in-progress"){
+                tasks.push(elm) ;
+            }
+        });
+        if(tasks.length <= 0 ){ return console.log(null);}
+        console.log(JSON.stringify(tasks , null , 2));
+    } catch (error) {
+        throw new error
+    }
+};
+
+const listDone = () => {
+    if(!fs.existsSync('tasks.json')){
+        return console.log("There is no file for todos!");        
+    }
+    const todos = JSON.parse(fs.readFileSync('tasks.json' , 'utf-8' ,  (err) => {
+        if(err){
+            throw new err ;
+        }
+    })) ;
+    let tasks = [] ; 
+    todos.forEach(elm => {
+        if(elm.status === "done"){
+            tasks.push(elm) ;
+        }
+    });
+    if(tasks.length <= 0 ){ return console.log(null);}
+    console.log(JSON.stringify(tasks , null , 2));
+}
+// listt all the todos or tasks 
+const listAll = () => {
+    if(!fs.existsSync('tasks.json')){
+        return console.log("There is no file for todos!");        
+    }
+    const todos = JSON.parse(fs.readFileSync('tasks.json' , 'utf-8' ,  (err) => {
+        if(err){
+            throw new err ;
+        }
+    })) ;
+
+    console.log(JSON.stringify(todos , null , 2));
+}
+
+// mark done functionality 
+const markDone = (todoId) => {
+    if(!todoId){
+        return console.log("⚠️️️️️️ Provide is please ⚠️️️️️️") ;
+    }
+    
+    const tasks = JSON.parse(fs.readFileSync('tasks.json' , 'utf-8' , (err) => {
+        if(err) {throw new err}
+    })) ;
+    if(!tasks){
+        console.log("Can't find any logs may be an server issue");
+    }
+    tasks.forEach((elm => {
+        if(elm.id == todoId){
+            elm.status = 'done' ;
+            return ;
+        }
+    }));
+    fs.writeFileSync('tasks.json' , JSON.stringify(tasks , null , 2))
+    console.log("✔️ Todo marked as Done ✔️");
+}
 // path operations dynamic routes for the functionality 
 const pathOperation = process.argv.slice(2)?.[0] ;
 
 switch (pathOperation){
-    case 'add' : 
+    case 'add' : //add functionality ✔️
         if(process.argv.slice(2).length >= 3){
             console.log('Enter just one task at a time');
             return ;
@@ -99,8 +181,8 @@ switch (pathOperation){
         const task = process.argv.slice(2)[1] ;
         addTask(task);
         break;
-    case 'list' :
-        if(process.argv.slice(2)[1] === 'todo'){
+    case 'list' : // list functionality ✔️ 
+        if(process.argv.slice(2)[1] === 'in-progress'){
             listTodo() ;
             break ;
         }
@@ -108,13 +190,13 @@ switch (pathOperation){
             listDone() ;
             break ;
         }
-        if(!process.argv.slice(2)[1]){
+        if(process.argv.slice(2)[1] === 'todo'){
             listAll() ;
             break ;
         }
         console.log("list task properly");
         break ; 
-    case 'update' :
+    case 'update' : // update functionalilty ✔️ 
         if(process.argv.slice(2).length >= 4){
             console.log('Enter just one task at a time');
             return ;
@@ -127,7 +209,7 @@ switch (pathOperation){
         const newtask =  process.argv.slice(2)[2] ;
         updateTask(taskId , newtask) ;
         break ;
-    case 'del' :
+    case 'del' : // delete functionality ✔️
         if(process.argv.slice(2).length > 2){
             console.log('Enter just one task at a time');
             return ;
@@ -138,6 +220,10 @@ switch (pathOperation){
         }
         const deltaskId = process.argv.slice(2)[1] ;
         deltask(deltaskId) ;
+        break ;
+    case 'mark-done':
+        const marktaskId = process.argv.slice(2)[1] ;
+        markDone(marktaskId) ;
         break ;
     default : 
         console.log("Invalid operation. Use 'add', 'list', 'update', or 'del'.");
