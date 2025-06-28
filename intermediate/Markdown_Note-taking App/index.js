5816e0448fab0fb29580ccc5f4928bd5288f0cbd
Debanjan Das <microsoft2420@gmail.com>
2025-06-29 00:09:07 +0530

    feat: update project structure with new routes, middleware, and styling; add shareHtml.css and shareFile.html

--- intermediate/Markdown_Note-taking App/index.js
+++ intermediate/Markdown_Note-taking App/index.js
@@ -1,10 +1,26 @@
 require('@dotenvx/dotenvx').config() // new dotenv '@dotenvx/dotenvx' anybody can go on npm.js and read about it more 
 
 const express = require('express')
+const path = require('path')
+const { appRouter } = require('./routes/app.routes.js')
 
 const app = express() ;
 const port = process.env.PORT ;
 
+
+
+
+// express middlewares and ejs and static files
+app.use(express.urlencoded({extended : true}))
+app.use(express.json())
+app.set('view engine', 'ejs') 
+app.use(express.static(path.join(__dirname , 'public')))
+
+
+// app routing 
+app.use('/api/v1/note' , appRouter);
+
+// app running in port 
 app.listen(port , () => {
     console.log(`App is running on port ${port}`);
 })
---
+++ intermediate/Markdown_Note-taking App/routes/app.routes.js
@@ -0,0 +1,7 @@
+const express = require('express')
+
+const appRouter = express.Router() ;
+
+module.exports = {
+    appRouter
+}
